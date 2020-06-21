918a6b7d2e4f42bc910c2987c4b0d14fa85db243
juancarlovieri <juan.c.vieri@gmail.com>
2020-06-21 11:42:28 +0700

    changed handle verification method

--- cfduel.js
+++ cfduel.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
+var crypto = require('crypto');
 var obj = JSON.parse(fs.readFileSync("handles.json", "utf8"));
 var map = new Map(Object.entries(obj));
 var temp = new Map();
+var time = new Map();
 
 function save(){
   var jsonObj = Object.fromEntries(map);
@@ -38,16 +40,15 @@ module.exports = {
       case 'regis':
         console.log('register');
         if(map.has(msg.from.username)){
-          bot.sendMessage(msg.chat.id, 'you are registered!');
-          break;
+          bot.sendMessage(msg.chat.id, 'you are reregistering!');
         }
         var args = msg.text.split(" ");
         if(args.length != 3){
           bot.sendMessage(msg.chat.id, 'that\'s not a valid thing');
           break;
         }
-        temp.set(msg.from.username, args[2])
-        bot.sendMessage(msg.chat.id, 'send a compile error to any of the problems here https://codeforces.com/contest/54 to verify it\'s you');
+        temp.set(msg.from.username, args[2]);
+        bot.sendMessage(msg.chat.id, 'submit ' + crypto.createHash('sha256').update(msg.from.username).digest('base64') + '  to any problem');
       break;
       case 'doneregis':
         if(temp.has(msg.from.username) == false){
@@ -57,16 +58,20 @@ module.exports = {
         var request = require('request');
         request('http://codeforces.com/api/user.status?handle=' + temp.get(msg.from.username) + '&from=1&count=1', function (error, response, body) {
           if (!error && response.statusCode == 200) {
-           var submission = JSON.parse(body);
-           console.log(submission.result[0]);
-           if(submission.result[0].contestId != 54 || submission.result[0].verdict != 'COMPILATION_ERROR'){
-            bot.sendMessage(msg.chat.id, 'send a compile error to any of the problems here https://codeforces.com/contest/54 to verify it\'s you');
-            return;
-           }
-           map.set(msg.from.username, temp.get(msg.from.username));
-           temp.delete(msg.from.username);
-           save();
-           bot.sendMessage(msg.chat.id, 'handle set!');
+            var submission = JSON.parse(body);
+            var request2 = require('request'); 
+            request2('http://codeforces.com/contest/' + submission.result[0].contestId + '/submission/' + submission.result[0].id, function(error, response, body){
+              var indx = body.indexOf(crypto.createHash('sha256').update(msg.from.username).digest('base64'));
+              if(indx < 0 || Date.now() - 3600000 > submission.result[0].creationTimeSeconds * 1000){
+                bot.sendMessage(msg.chat.id, 'submit ' + crypto.createHash('sha256').update(msg.from.username).digest('base64') + '  to any problem');
+                return;
+              } else{
+                map.set(msg.from.username, temp.get(msg.from.username));
+                temp.delete(msg.from.username);
+                save();
+                bot.sendMessage(msg.chat.id, 'registered!');
+              }
+            });
           }
         });
       break;
