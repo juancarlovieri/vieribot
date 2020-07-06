98bfcb5ffc609bfdba905bc0c8058f62ef2bcdfb
juancarlovieri <juan.c.vieri@gmail.com>
2020-07-06 23:16:27 +0700

    revert

--- reminder.js
+++ reminder.js
@@ -10,7 +10,7 @@ module.exports = {
       }
     }
     if(indx == -1){
-      bot.sendMessage(-1001265467717, 'internal error, contct developer');
+      bot.sendMessage(-1001265467717, 'internal error, contact developer');
       return;
     }
     setTimeout(function(){
